36a59af3fdc9ea459b8d61bc11683d19f10b457b
jtime <sgr918@gmail.com>
2011-07-05 17:46:45 +0800

    page 47 ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 ForumDemo::Application.routes.draw do
+  devise_for :users
+
   resources :boards do
 
   resources :posts
@@ -55,7 +57,7 @@ ForumDemo::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => "welcome#index"
-    root :to => "pages#welcome"
+    root :to => 'boards#index'
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
